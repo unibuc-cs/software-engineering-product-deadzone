bb360babae0792c418b4670a69aa0e324b154ef9
Sebastian Mihalache <sebimih@gmail.com>
2025-01-04 20:20:06 +0200

    Fix Random number generator

--- DeadZone/source/Random/Random.cpp
+++ DeadZone/source/Random/Random.cpp
@@ -2,8 +2,8 @@
 
 double Random::random01()
 {
-    std::random_device rd;
-    std::mt19937 gen(rd());
+    static std::random_device rd;
+	static std::mt19937 gen(rd());
     std::uniform_real_distribution<> dis(0.0, 1.0);
 
     return dis(gen);
@@ -11,8 +11,8 @@ double Random::random01()
 
 int Random::randomInt(int min, int max)
 {
-	std::random_device rd;
-	std::mt19937 gen(rd());
+	static std::random_device rd;
+	static std::mt19937 gen(rd());
 	std::uniform_int_distribution<> dis(min, max);
 
 	return dis(gen);
@@ -20,8 +20,9 @@ int Random::randomInt(int min, int max)
 
 std::mt19937 Random::randomGen()
 {
-	std::random_device rd;
-	std::mt19937 gen(rd());
+	static std::random_device rd;
+	static std::mt19937 gen(rd());
+
 	return gen;
 }
 
